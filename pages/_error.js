f1d7c21778730efb35a21b6db67215ea993e6205
SkeletalWaffles <acrystalflame@gmail.com>
2020-08-05 15:37:26 -0600

    Added further styling to 404 page and closed issue.

--- pages/_error.js
+++ pages/_error.js
@@ -10,9 +10,9 @@ const Error = ({ statusCode }) => {
                 <div className='error-msg'>Nothin' but cobwebs here!</div>
             )
         } else if (statusCode) {
-            return <div className='error-msg'>A <b>{statusCode}</b> happened on the server.</div>
+            return <div className='error-msg'>An error happened on the server.</div>
         } else {
-            return <div className='error-msg'>An error happened with the client</div>
+            return <div className='error-msg'>An error happened with the client.</div>
         }
     }
 
@@ -20,6 +20,7 @@ const Error = ({ statusCode }) => {
         <div className='error-container'>
             <div className='error-heading'>{statusCode || '?'}</div>
             {chooseErrorMessage()}
+            
             <button
                 className='return-home'
                 onClick={() => router.push('/')}

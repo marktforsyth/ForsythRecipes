b7f17d0cb56501d5da9bfe62f80135b37f555d97
Mark Forsyth <acrystalflame@gmail.com>
2020-12-21 18:56:42 +0000

    Switched to TinyMCE, will expriment more.

---
+++ components/backup/create-recipe.js
@@ -0,0 +1,72 @@
+import React, { useState } from 'react'
+import { useAuth0 } from '@auth0/auth0-react'
+import { NextSeo } from 'next-seo'
+import axios from 'axios'
+// import moment from 'moment'
+import dynamic from 'next/dynamic'
+import { convertToRaw, EditorState } from 'draft-js'
+import draftToHtml from 'draftjs-to-html'
+
+const EditorNoSSR = dynamic(
+  () => import('react-draft-wysiwyg').then(
+    module => module.Editor
+  ),
+  { ssr: false }
+)
+
+const CreateRecipe = () => {
+  const { isAuthenticated, loginWithRedirect, user } = useAuth0()
+
+  if (!isAuthenticated) {
+    return <p>Trying to login...</p>
+
+    loginWithRedirect()
+  }
+
+  const [title, setTitle] = useState()
+  const [body, setBody] = useState()
+  // const [editorState, setEditorState] = useState(EditorState.createEmpty())
+  const editorState = EditorState.createEmpty()
+
+  const submitForm = (event) => {
+    event.preventDefault()
+    
+    // axios.post('/api/recipes', {
+    //   title,
+    //   body,
+    //   creator: user.name
+    // })
+    console.log('Title: ', title)
+    console.log('Body: ', htmlData)
+  }
+
+  return (
+    <div>
+      <NextSeo title={'New Recipe'} />
+      <form onSubmit={(event) => submitForm(event)}>
+        <h2>Recipe Title:</h2>
+        <input
+          placeholder='Title...'
+          onChange={(event) => setTitle(event.target.value)}
+        ></input>
+
+        <h2>Recipe Content:</h2>
+        <EditorNoSSR
+          editorState={ editorState }
+          onEditorStateChange={(event) => {
+            let rawContentState = convertToRaw(editorState.getCurrentContent());
+            let htmlData = draftToHtml(rawContentState)
+            console.log(htmlData)
+
+            console.log(event)
+          }}
+          // onChange={(event) => setBody(event.target.value)}
+        />
+
+        <button type='submit'>Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default CreateRecipe
--- pages/create-recipe.js
+++ pages/create-recipe.js
@@ -2,29 +2,7 @@ import React, { useState } from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
 import { NextSeo } from 'next-seo'
 import axios from 'axios'
-// import moment from 'moment'
-import dynamic from 'next/dynamic'
-
-const EditorNoSSR = dynamic(
-  () => import('react-draft-wysiwyg').then(
-    module => module.Editor
-  ),
-  { ssr: false }
-)
-
-const convertToRawNoSSR = dynamic(
-  () => import('draft-js').then(
-    module => module.converToRaw
-  ),
-  { ssr: false }
-)
-
-const draftToHtml = dynamic(
-  () => import('draftjs-to-html').then(
-    module => module
-  ),
-  { ssr: false }
-)
+import { Editor } from '@tinymce/tinymce-react'
 
 const CreateRecipe = () => {
   const { isAuthenticated, loginWithRedirect, user } = useAuth0()
@@ -38,20 +16,27 @@ const CreateRecipe = () => {
   const [title, setTitle] = useState()
   const [body, setBody] = useState()
 
-  const submitForm = (event) => {
-    event.preventDefault()
-    
-    axios.post('/api/recipes', {
-      title,
-      body,
-      creator: user.name
-    })
+  const handleEditorChange = (content, editor) => {
+    console.log('Behold: ', content)
+    console.log(editor)
   }
+
+  // const submitForm = (event) => {
+  //   event.preventDefault()
+    
+  //   // axios.post('/api/recipes', {
+  //   //   title,
+  //   //   body,
+  //   //   creator: user.name
+  //   // })
+  //   console.log('Title: ', title)
+  //   console.log('Body: ', htmlData)
+  // }
 
   return (
     <div>
       <NextSeo title={'New Recipe'} />
-      <form onSubmit={(event) => submitForm(event)}>
+      {/* <form onSubmit={(event) => submitForm(event)}>
         <h2>Recipe Title:</h2>
         <input
           placeholder='Title...'
@@ -59,12 +44,23 @@ const CreateRecipe = () => {
         ></input>
 
         <h2>Recipe Content:</h2>
-        <EditorNoSSR
-          // onChange={(event) => setBody(event.target.value)}
-        />
 
-        <button type='submit'>Submit</button>
-      </form>
+
+        <button type='submit'>Submit</button> 
+      </form> */}
+      <Editor
+        init={{
+          height: 500,
+          plugins: 'help image autolink',
+          toolbar:
+             'undo redo | formatselect | bold italic underline forecolor backcolor | \
+             alignleft aligncenter alignright alignjustify | \
+             bullist numlist | image | help',
+          content_style: '@import url(\'https://fonts.googleapis.com/css2?family=Montserrat:wght@300;500&display=swap\');',
+          font_formats: 'Montserrat=montserrat, sans-serif; body { font-weight: 300 };'
+        }}
+        onEditorChange={(content, editor) => handleEditorChange(content, editor)}
+      />
     </div>
   )
 }
